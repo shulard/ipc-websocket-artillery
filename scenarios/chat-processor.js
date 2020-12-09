29040a579fa8b5162e7880af093bb4774d45d898
Stéphane HULARD <s.hulard@chstudio.fr>
2020-12-09 10:55:05 +0100

    Log websocket received message.

--- scenarios/chat-processor.js
+++ scenarios/chat-processor.js
@@ -7,5 +7,9 @@ module.exports = {
 function initializeContext(context, events, done) {
     context.vars['name'] = faker.name.findName();
 
+    context.ws.on('message', function(receivedMessage) {
+        console.log(receivedMessage);
+    });
+
     return done();
 }
